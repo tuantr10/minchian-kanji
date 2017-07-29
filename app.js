18c8b9acdc726406790dee0bf9536c8266adc6f7
Tuan Dung TRAN <mcafee92@gmail.com>
2017-07-30 01:05:39 +0900

    fix nodemon

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 var express = require('express')
+var mongoose = require('mongoose')
+var config = require('./core/config')
+
 var app = express()
 var ECT = require('ect')
 var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' })
@@ -6,12 +9,14 @@ var ectRenderer = ECT({ watch: true, root: __dirname + '/views', ext : '.ect' })
 app.set('view engine', 'ect')
 app.engine('ect', ectRenderer.render)
 
-// respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
-  res.render('index')
+	res.render('index')
 })
+
+var mongodb = config.mongo
+mongoose.connect(mongodb);
 
 //Launch listening server on port 8081
 app.listen(8081, function () {
-  console.log('app listening on port 8081!')
+	console.log('app listening on port 8081!')
 })

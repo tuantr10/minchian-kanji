a833bb4963c66923ca442ebdc4255295e6cdc709
Tuan Dung TRAN <mcafee92@gmail.com>
2017-08-28 00:30:16 +0900

    move desktop app to one separate module

--- app.js
+++ app.js
@@ -1,28 +1,13 @@
 var express = require('express')
 var mongoose = require('mongoose')
 var config = require('./core/config')
-
 var app = express()
-var ect = require('ect')
-var viewPath = __dirname + '/apps/desktop/views'
 
 var mongodb = config.db
-app.set('view engine', 'ect')
-app.set('views', viewPath);
-
-app.engine('ect', ect({
-		//cache: !config.debug,
-		root: viewPath,
-		ext: '.ect',
-		open: '{%',
-		close: '%}',
-	}).render);
-
-app.get('/', function (req, res) {
-	res.render('index')
-})
-
 mongoose.connect(mongodb)
+
+var desktopApp = require('./apps/desktop/module')
+desktopApp.init(app)
 
 //Launch listening server on port 8081
 app.listen(8081, function () {
